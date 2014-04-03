4bbe73debf87a6bf88060093aa06cd0e0635711e
tato784 <tato_saxiashvili@mail.ru>
2014-04-03 18:04:53 +0400

    started signalR implementation

---
+++ ChatRoom/ChatRoom.Server.SignalR/ChatServer/ChatRoomHub.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatRoom.Server.SignalR.ChatServer {
+	public class ChatRoomHub : Hub {
+
+		private readonly Messanger _messenger;
+
+		#region BaseMethods
+
+		public override System.Threading.Tasks.Task OnConnected() {
+			//Get user id from qs
+			int userId = 7889;
+			_messenger.ComeOnline(Context.ConnectionId, userId);
+
+			return base.OnConnected();
+		}
+
+		public override System.Threading.Tasks.Task OnDisconnected() {
+			return base.OnDisconnected();
+		}
+
+		public override System.Threading.Tasks.Task OnReconnected() {
+			_messenger.WentOffline(Context.ConnectionId);
+
+			return base.OnReconnected();
+		}
+
+		#endregion
+
+		public void JoinRoom(IEnumerable<int> userIds) {
+			var room = _messenger.CreateRoom(userIds);
+			var userConnections = _messenger.GetConnectionIdsByID(userIds);
+			Groups.Add(Context.ConnectionId, room);
+			foreach(var item in userConnections) {
+				Groups.Add(item, room);
+			}
+		}
+
+		public void PostInRoom(string roomName, string comment) {
+			Clients.Group(roomName, Context.ConnectionId).sendRoomMessage(roomName, comment);
+		}
+	}
+}
---
+++ ChatRoom/ChatRoom.Server.SignalR/ChatServer/Messanger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatRoom.Server.SignalR.ChatServer {
+	public class Messanger {
+		private static readonly Lazy<Messanger> _instance = new Lazy<Messanger>(() => new Messanger());
+
+		private static readonly Dictionary<string, IEnumerable<int>> _chatRooms = new Dictionary<string, IEnumerable<int>> { };
+		private static readonly Dictionary<string, int> _users = new Dictionary<string, int> { };
+
+		private Messanger() { }
+
+		private static Messanger Instance { get { return _instance.Value; } }
+
+		public string CreateRoom(IEnumerable<int> userIds) {
+			var roomToken = Guid.NewGuid().ToString("N") + Guid.NewGuid().ToString("N");
+
+			//((List<int>)userIds).Add(session.UserID);
+			_chatRooms.Add(roomToken, userIds);
+
+			return roomToken;
+		}
+
+		public void ComeOnline(string connectionId, int userId) {
+			_users.Add(connectionId, userId);
+		}
+
+		public void WentOffline(string connectionId) {
+			_users.Remove(connectionId);
+		}
+
+		public IEnumerable<string> GetConnectionIdsByID(IEnumerable<int> ids) {
+			return from x in _users
+				   where ids.Contains(x.Value)
+				   select x.Key;
+		}
+	}
+}
