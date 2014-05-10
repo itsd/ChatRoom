862ff55561e14c4dbf4f2f78d0ab0d4962f39c13
tato784 <tato_saxiashvili@mail.ru>
2014-05-11 02:07:22 +0400

    start adding profile settings

--- ChatRoom/ChatRoom/App/controllers/profileController.js
+++ ChatRoom/ChatRoom/App/controllers/profileController.js
@@ -1,3 +1,13 @@
-﻿app.controller('profileController', function ($scope, $http) {
+﻿app.controller('profileController', function ($scope, $http, sessionService) {
+	$scope.isEdit = false;
+	$scope.username = sessionService.user.username;
+	$scope.name = sessionService.user.name;
 
+	$scope.showEdit = function () {
+		$scope.isEdit = true;
+	};
+
+	$scope.saveSettings = function () {
+		$scope.isEdit = false;
+	}
 });
