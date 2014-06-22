ad5671935328e1a133ab6eb1b7f0475750a493f2
tato784 <tato_saxiashvili@mail.ru>
2014-06-23 02:41:04 +0400

    share controller

---
+++ ChatRoom/ChatRoom/App/controllers/shareController.js
@@ -0,0 +1,7 @@
+﻿app.controller('shareController', function ($scope, $http, signalrService, sessionService) {
+	//$scope.session = sessionService;
+
+	$scope.session = true;
+
+	//session.isAuthenticated
+});
